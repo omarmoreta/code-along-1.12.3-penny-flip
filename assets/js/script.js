02789c0fabbe02ea408234ed31e3adde17f6abdb
Omar <Omar>
2021-08-24 13:17:46 -0400

    Coin Flipper

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,66 @@
-// TODO: Declare any global variables we need
+let head = 0;
+let tail = 0;
 
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Hi");
+  let flip = document.getElementById("flip");
+  flip.addEventListener("click", function () {
+    console.log("You Flipped Heads");
+    let flipped = Math.random() < 0.5;
+    console.log(flipped);
+    if (flipped) {
+      document.getElementById("penny_img").src =
+        "./assets/images/penny-heads.jpg";
+      document.getElementById("message").textContent = "You Flipped Heads!";
+      head++;
+      console.log(head);
+    } else {
+      document.getElementById("penny_img").src =
+        "./assets/images/penny-tails.jpg";
+      document.getElementById("message").textContent = "You Flipped Tails!";
+      tail++;
+      console.log(tail);
+    }
+    let total = head + tail;
+    let headpercentage = 0;
+    let tailpercentage = 0;
+    if (total > 0) {
+      headpercentage = Math.round((head / total) * 100);
+      tailpercentage = Math.round((tail / total) * 100);
+      document.getElementById("heads").textContent = head;
+      document.getElementById("heads-percent").textContent =
+        headpercentage + "%";
+      document.getElementById("tails").textContent = tail;
+      document.getElementById("tails-percent").textContent =
+        tailpercentage + "%";
+    }
+  });
+  let clear = document.getElementById("clear");
+  clear.addEventListener("click", function () {
+    console.log("cleared");
+    document.getElementById("message").textContent = "Lets Get Rolling!";
+    let headpercentage = 0;
+    let tailpercentage = 0;
+    head = 0;
+    tail = 0;
+    document.getElementById("heads").textContent = head;
+    document.getElementById("heads-percent").textContent = headpercentage + "%";
+    document.getElementById("tails").textContent = tail;
+    document.getElementById("tails-percent").textContent = tailpercentage + "%";
+  });
 
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+  // Flip Button Click Handler
+  // todo: Determine flip outcome
+  // todo: Update image and status message in the DOM
 
-    // TODO: Add event listener and handler for flip and clear buttons
+  // Update the scorboard
+  // todo: Calculate the total number of rolls/flips
+  // Make variables to track the percentages of heads and tails
+  // todo: Use the calculated total to calculate the percentages
+  // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
+  // todo: Update the display of each table cell
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
-})
+  // Clear Button Click Handler
+  // todo: Reset global variables to 0
+  // todo: Update the scoreboard (same logic as in flip button click handler)
+});
